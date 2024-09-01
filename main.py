92240149a6936075ea9d3958b5425e949e231de9
zhujiang <dave1221@163.com>
2024-09-01 21:36:46 +0800

    [init]

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+Created on 2024/9/1 21:10
+@author: Zhu Jiang
+"""
+
+
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    print("I am inside hello world")
+    return "Hello, World!"
+
+@app.route('/echo/<name>')
+def echo(name):
+    print(f"this was placed in the url: new-{name}")
+    return jsonify({'new-name': name})
+
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=8080, debug=True)
