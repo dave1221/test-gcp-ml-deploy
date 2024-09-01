c9a1a9e3aabe736b2afad4229908021cb0b24264
zhujiang <dave1221@163.com>
2024-09-02 00:52:06 +0800

    [test continuous delivery]

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     print("I am inside hello world")
-    return "Hello, World! CDC 2024"
+    return "Hello, World! CDC 2024 Project"
 
 @app.route('/echo/<name>')
 def echo(name):
